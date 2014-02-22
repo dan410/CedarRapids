0371f8311599204e5c366e94110fbdbdd950a808
dan410 <dan410@gmail.com>
2014-02-22 13:20:13 -0600

    Added Uniform distribution to histogram.R

--- histogram.R
+++ histogram.R
@@ -1,2 +1,14 @@
-x <- rnorm(1000)
-hist(x, freq = FALSE, main = "Awesome Histogram")
+
+# Normal Distribution
+x <- rnorm(100)
+print(x)
+pdf("normal-hist.pdf")
+hist(x, freq = FALSE, main = "Normal Distribution")
+dev.off()
+
+# Uniform Distribution
+x <- runif(100)
+print(x)
+pdf("uniform-hist.pdf")
+hist(x, freq = FALSE, main = "Uniform Distribution")
+dev.off()
