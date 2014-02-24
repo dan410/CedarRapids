fbc84ef73271b54b4f0a8b1e0ed6b27fa6020b7d
dan410 <dan410@gmail.com>
2014-02-23 18:04:46 -0600

    added x-axis label to histograms

--- histogram.R
+++ histogram.R
@@ -6,12 +6,12 @@
 x <- rnorm(100)
 print(x)
 pdf("normal-hist.pdf")
-hist(x, freq = FALSE, main = "Normal Distribution")
+hist(x, freq = FALSE, main = "Normal Distribution", xlab = "values")
 dev.off()
 
 # Uniform Distribution
 x <- runif(100)
 print(x)
 pdf("uniform-hist.pdf")
-hist(x, freq = FALSE, main = "Uniform Distribution")
+hist(x, freq = FALSE, main = "Uniform Distribution", xlab = "values")
 dev.off()
